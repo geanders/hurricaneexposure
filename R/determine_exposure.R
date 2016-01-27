ed0dce2c8fb7775f4e8f77cc0570d0c127ed18b1
Brooke Anderson <geanderson@jhsph.edu>
2016-01-27 11:24:22 -0700

    Work on multi-county exposure df generator

--- R/determine_exposure.R
+++ R/determine_exposure.R
@@ -30,9 +30,9 @@
 #'    distance between the county center and the storm track on
 #'    that date and time, and cumulative rainfall for the week-long
 #'    period centered at the date of closest approach to the county.
-exposed_storms <- function(counties, start_year, end_year,
+rain_storms <- function(counties, start_year, end_year,
                            rain_limit, dist_limit){
-        df <- dplyr::filter(closest_dist,
+        rain_storm_df <- dplyr::filter(closest_dist,
                             fips %in% counties &
                                     year(closest_date) >= start_year &
                                     year(closest_date) <= end_year &
@@ -40,14 +40,46 @@ exposed_storms <- function(counties, start_year, end_year,
                 dplyr::left_join(storm_rains,
                                  by = c("storm_id", "fips")) %>%
                 dplyr::filter(tot_precip >= rain_limit)
-        return(df)
+        return(rain_storm_df)
+}
+
+#' Create multi-county community exposures
+#'
+#' This function takes a rain-exposure dataframe for counties,
+#'    as created by the \code{rain_storms} function, as well as
+#'    a list object with the counties in each multi-county
+#'    community, and returns a community-level dataframe of
+#'    "exposed" storms, based on the rainfall and distance
+#'    thresholds used to create the rain-exposure dataframe.
+#'
+#' @param rain_storm_df A dataframe of county-level "exposed"
+#'    storms, as created by \code{rain_storms}.
+#' @param community_list A list object with an element for each
+#'    community and a vector of FIPS codes for all counties within
+#'    each community
+#'
+#' @return Returns the same type dataframe as \code{rain_storms},
+#'    but with storms listed by community instead of county.
+#'
+#' @examples
+#' communities <- data.frame(commun = c(rep("ny", 6), "no", "new"),
+#'                          fips = c("36005", "36047", "36061",
+#'                                   "36085", "36081", "36119",
+#'                                   "22071", "51700"))
+#' rain_storm_df <- rain_storms(counties = communities$fips,
+#'                              start_year = 1995, end_year = 2005,
+#'                              rain_limit = 20, dist_limit = 100)
+multi_county <- function(rain_storm_df, communities){
+        multi_df <- left_join(rain_storm_df, communities,
+                              by = "fips")
+
 }
 
 rain_exposure <- function(counties, start_year, end_year,
                           rain_limit, dist_limit,
                           out_dir){
 
-        df <- exposed_storms(counties = counties,
+        df <- rain_storms(counties = counties,
                              start_year = start_year,
                              end_year = end_year,
                              rain_limit = rain_limit,
