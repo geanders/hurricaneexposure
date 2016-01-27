5ba9baec1c68cbbabf4270dda5bd2bfdc208cd50
Brooke Anderson <geanderson@jhsph.edu>
2016-01-27 10:39:23 -0700

    Work on function to create dataframe of storm exposure

--- R/determine_exposure.R
+++ R/determine_exposure.R
@@ -5,17 +5,53 @@
 # dist_limit <- 100
 # out_dir <- "~/tmp/hurr_rain"
 
-rain_exposure <- function(counties, start_year, end_year,
-                          rain_limit, dist_limit,
-                          out_dir){
+#' Create subset of storms for FIPS
+#'
+#' This function takes a list of county FIPS, boundaries on
+#'    starting and ending years, and thresholds for distance
+#'    and the week-window rainfall and returns a dataframe
+#'    with the subset of storms meeting those criteria for
+#'    each of the listed counties.
+#'
+#' @param counties Character string of the FIPS codes for
+#'    counties to create data for
+#' @param start_year Four-digit integer with first year to consider.
+#' @param end_year Four-digit integer with last year to consider.
+#' @param rain_limit Minimum of rainfall for the week-long period
+#'    centered at the storm's closest date to consider county
+#'    "exposed" to the storm.
+#' @param dist_limit Maximum distance for the closest distance
+#'    between the county center and the storm track to consider
+#'    the county "exposed" to the storm.
+#'
+#' @return Returns a dataframe with a row for each county-storm
+#'    pair and with columns giving the storm's ID, county FIPS,
+#'    date and time of closest approach of that storm to that county,
+#'    distance between the county center and the storm track on
+#'    that date and time, and cumulative rainfall for the week-long
+#'    period centered at the date of closest approach to the county.
+exposed_storms <- function(counties, start_year, end_year,
+                           rain_limit, dist_limit){
         df <- dplyr::filter(closest_dist,
                             fips %in% counties &
-                              year(closest_date) >= start_year &
-                              year(closest_date) <= end_year &
-                              storm_dist <= dist_limit) %>%
+                                    year(closest_date) >= start_year &
+                                    year(closest_date) <= end_year &
+                                    storm_dist <= dist_limit) %>%
                 dplyr::left_join(storm_rains,
                                  by = c("storm_id", "fips")) %>%
                 dplyr::filter(tot_precip >= rain_limit)
+        return(df)
+}
+
+rain_exposure <- function(counties, start_year, end_year,
+                          rain_limit, dist_limit,
+                          out_dir){
+
+        df <- exposed_storms(counties = counties,
+                             start_year = start_year,
+                             end_year = end_year,
+                             rain_limit = rain_limit,
+                             dist_limit = dist_limit)
 
         start_date <- as.Date(paste0(start_year, "0101"),
                               format = "%Y%m%d")
