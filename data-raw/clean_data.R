57f45249654030b8b4ae73fe79bbacaa379464b3
Brooke Anderson <geanderson@jhsph.edu>
2016-05-23 19:41:34 -0600

    Update to get hurr_tracks directly from online.
    Now, we process the hurricane tracks by pulling directly from the extended best tracks data. (Note: there are variables from this dataset that we aren't using yet but could in the future.)

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -28,18 +28,31 @@ colnames(hurr_tracks) <- c("storm_id", "storm_name", "month", "day",
                           paste("radius_50", c("ne", "se", "sw", "nw"), sep = "_"),
                           paste("radius_64", c("ne", "se", "sw", "nw"), sep = "_"),
                           "storm_type", "distance_to_land", "final")
+format_longitude <- function(lon){
+        lon <- as.character(lon)
+        names(lon) <- NULL
+        lon <- sapply(lon, function (x)  sub("^\\s+", "", x))
+        out <- strsplit(lon, " ")
+        out <- sapply(out, function(x) x[[1]])
+        out <- -1 * as.numeric(out)
+        return(out)
+}
 hurr_tracks <- select(hurr_tracks, -storm_id) %>%
-        mutate(storm_id = paste(gsub(" ", "", storm_name), year, sep = "-"),
+        mutate(storm_name = as.character(storm_name),
+               storm_name = paste0(substr(storm_name, 1, 1),
+                                  substr(tolower(storm_name), 2,
+                                         nchar(storm_name))),
+               storm_id = paste(gsub(" ", "", storm_name), year, sep = "-"),
+               longitude = format_longitude(longitude),
                month = sprintf("%02d", month),
                day = sprintf("%02d", day),
                hour = sprintf("%02d", hour),
-               minute = "00",
-               longitude = -1 * longitude) %>%
+               minute = "00") %>%
         rename(wind = max_wind) %>%
         select(storm_id, year, month, day, hour, minute, latitude, longitude,
                wind)
 hurr_tracks <- unite_(hurr_tracks, "date",
                       c("year", "month", "day", "hour", "minute"),
                       sep = "")
-use_data(hurr_tracks)
+use_data(hurr_tracks, overwrite = TRUE)
 
