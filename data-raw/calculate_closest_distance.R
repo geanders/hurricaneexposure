2a545b7e331b6d309def723abe37254d4cb0235c
Brooke Anderson <geanderson@jhsph.edu>
2016-06-08 15:10:54 -0600

    Convert closest_date to local time and add local time

--- data-raw/calculate_closest_distance.R
+++ data-raw/calculate_closest_distance.R
@@ -1,6 +1,7 @@
 library(sp)
 library(dplyr)
 library(lubridate)
+library(hurricaneexposure)
 
 data(county_centers, package = "hurricaneexposure")
 data(hurr_tracks, package = "hurricaneexposure")
@@ -19,7 +20,7 @@ calc_closest_dist <- function(this_storm = "Floyd-1999"){
         # Calculate distance from county center to storm path
         storm_county_distances <- spDists(
                 as.matrix(county_centers[,c("longitude", "latitude")]),
-                as.matrix(storm_tracks[,c("lon", "lat")]),
+                as.matrix(storm_tracks[,c("lon", "phi")]),
                 longlat = TRUE) # Return distance in kilometers
 
         min_locs <- apply(storm_county_distances, 1, which.min)
@@ -37,7 +38,7 @@ calc_closest_dist <- function(this_storm = "Floyd-1999"){
 
         closest_dist <- mutate(county_centers,
                                closest_date = storm_tracks$date[min_locs],
-                               storm_lat = storm_tracks$lat[min_locs],
+                               storm_lat = storm_tracks$phi[min_locs],
                                storm_long = storm_tracks$lon[min_locs],
                                storm_dist = min_dists) %>%
                 filter(state_name %in% study_states) %>%
@@ -53,27 +54,18 @@ hurrs <- as.character(unique(hurr_tracks$storm_id))
 
 closest_dist <- lapply(hurrs, calc_closest_dist)
 closest_dist <- do.call("rbind", closest_dist)
+
+library(countytimezones)
+closest_dist <- add_local_time(df = closest_dist,
+                     datetime_colname = "closest_date",
+                     fips = closest_dist$fips,
+                     include_tz = FALSE) %>%
+        dplyr::rename(closest_time_utc = closest_date,
+               closest_date = local_date) %>%
+        mutate(closest_time_utc = ymd_hm(closest_time_utc)) %>%
+        mutate(closest_time_utc = format(closest_time_utc,
+                                         "%Y-%m-%d %H:%M"))
+
 use_data(closest_dist, overwrite = TRUE)
 
 
-# # Plotting
-# map("state", region = study_states,
-#     interior = FALSE,
-#     mar = c(0, 0, 0, 0), ylim = c(24, 48), xlim = c(-107, -60))
-#
-# map("state", region = study_states,
-#     boundary = FALSE, col = "gray", add = TRUE)
-#
-# points(closest_dist$longitude, closest_dist$latitude, pch = ".",
-#        col = "gray")
-#
-# box(col = "lightgray")
-#
-# with(closest_dist, segments(longitude, latitude,
-#                                   storm_long, storm_lat, col = rgb(0, 0, 1, 0.2)))
-# points(storm_tracks$lon, storm_tracks$lat,
-#        pch = 21, cex = 0.8, bg = "yellow", col = "black", type = "l")
-#
-# pos_by_day <- subset(storm_tracks, as.POSIXlt(date)$hour == 0)
-# with(pos_by_day[7:11,], text(lon, lat, format(date, "%b %d, %Y"),
-#                              pos = 4, cex = 0.8))
