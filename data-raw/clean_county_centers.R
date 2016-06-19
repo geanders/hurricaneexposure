0bb8150ac15bed713d6b86ff1d5220bb25147e82
Brooke Anderson <geanderson@jhsph.edu>
2016-06-19 17:43:01 -0600

    Close #35

--- data-raw/clean_county_centers.R
+++ data-raw/clean_county_centers.R
@@ -1,12 +1,14 @@
 library(dplyr)
 library(devtools)
+library(stringi)
 
 # Read in and clean up `county_centers`
 county_centers <- read.csv(paste0("http://www2.census.gov/geo/docs/reference/",
                                   "cenpop2010/county/CenPop2010_Mean_CO.txt"),
                            as.is = TRUE) %>%
         mutate(fips = paste0(sprintf("%02d", STATEFP),
-                             sprintf("%03d", COUNTYFP))) %>%
+                             sprintf("%03d", COUNTYFP)),
+               COUNAME = stri_trans_general(COUNAME, "latin-ascii")) %>%
         select(fips, COUNAME, STNAME, POPULATION, LATITUDE, LONGITUDE) %>%
         rename(county_name = COUNAME,
                state_name = STNAME,
