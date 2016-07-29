60628456445e42209e421e29ce64e6a4c2d29597
jferreri <joshua.m.ferreri@gmail.com>
2016-07-29 14:19:44 -0600

    pull dade timeseries

---
+++ writing/DraftExposurePaper/dade_storm_plot.R
@@ -0,0 +1,5 @@
+fips <- "12086"
+county_timeseries(fips, percent_coverage = 0,
+                  date_min = "1988-01-01", date_max = "2011-12-31",
+                  var = "PRCP", #out_directory = "~/frances_ex/"
+                  out_directory = "~/Documents/hurricaneexposure/writing/DraftExposurePaper/dade_data/")
