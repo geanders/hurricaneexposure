8027e593c85b1eb0f6c6374b18c09aef6d0e5e07
jferreri <joshua.m.ferreri@gmail.com>
2016-07-29 14:24:57 -0600

    begin frances code

--- writing/DraftExposurePaper/dade_storm_plot.R
+++ writing/DraftExposurePaper/dade_storm_plot.R
@@ -3,3 +3,6 @@ county_timeseries(fips, percent_coverage = 0,
                   date_min = "1988-01-01", date_max = "2011-12-31",
                   var = "PRCP", #out_directory = "~/frances_ex/"
                   out_directory = "~/Documents/hurricaneexposure/writing/DraftExposurePaper/dade_data/")
+frances_dir <- "~/Documents/hurricaneexposure/writing/DraftExposurePaper/dade_data/"
+county_weather <- readRDS(paste0(frances_dir, fips, ".rds"))
+county_weather <-
