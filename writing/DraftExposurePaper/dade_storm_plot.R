b749ffb98cc7bc2b5416e389a650810cbe015a38
Brooke Anderson <geanderson@jhsph.edu>
2016-08-02 14:04:03 -0400

    Make some tweaks to the Miami-Dade plot

--- writing/DraftExposurePaper/dade_storm_plot.R
+++ writing/DraftExposurePaper/dade_storm_plot.R
@@ -37,8 +37,8 @@ all_fips <- c(all_fips, as.integer(12025))
 check_dates[check_dates$fips == 12086, "fips"] <- 12025
 
 dade_rain <- data.table::fread(
-        "/Users/joshuaferreri/Documents/nasa_precip_export_2.txt",
-        #"data-raw/nasa_precip_export_2.txt",
+        #"/Users/joshuaferreri/Documents/nasa_precip_export_2.txt",
+        "~/Documents/CSU2016/hurricaneproject/hurricaneexposuredata/data-raw/nasa_precip_export_2.txt",
         header = TRUE,
         select = c("county", "year_month_day", "precip", "precip_max")) %>%
         dplyr::filter(county %in% all_fips,
@@ -66,11 +66,13 @@ dade_rain <- dade_rain %>%
 fips <- "12086"
 county_timeseries(fips, percent_coverage = 0,
                   date_min = "1988-01-01", date_max = "2011-12-31",
-                  var = "PRCP", #out_directory = "~/frances_ex/"
-                  out_directory = "~/Documents/hurricaneexposure/writing/DraftExposurePaper/dade_data/")
+                  var = "PRCP",
+                  out_directory = "~/dade_data/")
+                  #out_directory = "~/Documents/hurricaneexposure/writing/DraftExposurePaper/dade_data/")
 
-frances_dir <- "~/Documents/hurricaneexposure/writing/DraftExposurePaper/dade_data/"
-county_weather <- readRDS(paste0(frances_dir, fips, ".rds"))
+# miami_dade_dir <- "~/Documents/hurricaneexposure/writing/DraftExposurePaper/dade_data/"
+dade_dir <- "dade_data/"
+county_weather <- readRDS(paste0(dade_dir, fips, ".rds"))
 
 county_weather <- county_weather %>%
         filter(date %in% seq(from = as.Date("1988-01-01"), to = as.Date("2011-12-31"), by = 1))
@@ -96,9 +98,13 @@ county_weather %>%
                   tot_precip = sum(precip),
                   prcp_reporting = mean(prcp_reporting)) %>%
         ggplot(aes(x = monitor_rain, y = tot_precip)) +
+        geom_hline(aes(yintercept = 75), color = "lightgray") +
+        geom_vline(aes(xintercept = 75), color = "lightgray") +
         geom_abline(aes(intercept = 0, slope = 1), color = "gray", alpha = 0.5) +
-        geom_point(aes(size = prcp_reporting), alpha = 0.2) +
-        geom_text(aes(x = monitor_rain + 100, label = storm_id)) +
+        # geom_point(aes(size = prcp_reporting), alpha = 0.2) +
+        geom_point(alpha = 0.5) +
+        # geom_text(aes(x = monitor_rain + 100, label = storm_id)) +
+        xlim(c(0, 275)) + ylim(c(0, 275)) +
         theme_few() +
         scale_size_continuous(guide = "none") +
         xlab("Rainfall (mm) based on \naveraged county monitors") +
