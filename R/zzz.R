6fcfdc1f7dfd0f03d4eee36af87cf900f1909800
Brooke Anderson <brooke.anderson@colostate.edu>
2017-01-17 16:08:34 -0700

    Update .onLoad to try to work with drat repo

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(libname) {
+.onLoad <- function(libname, pkgname) {
         repos = getOption("repos")
         repos["<NAME_REPO>"] = "http://geanders.github.io/drat"
         options(repos = repos)
