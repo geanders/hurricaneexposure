786ec93d6d42965a4b5b2a03cc3d52a8973718e9
Brooke Anderson <brooke.anderson@colostate.edu>
2017-01-19 12:37:55 -0700

    Add to zzz.R to check for `hurricaneexposuredata`

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,14 @@
+.pkgglobalenv <- new.env(parent=emptyenv())
+
 .onLoad <- function(libname, pkgname) {
-        repos <- getOption("repos")
-        repos["my_repo"] <- "http://geanders.github.io/drat"
-        options(repos = repos)
-        invisible(repos)
+  has_data_package <- requireNamespace("hurricaneexposuredata")
+  if(!has_data_package){
+    packageStartupMessage(paste("To use this package, you must install the",
+                                "hurricaneexposuredata package."))
+    packageStartupMessage(paste("To install that package, run",
+                                "`install.packages('hurricaneexposuredata',",
+                                "repos = 'https://geanders.github.io/drat/',",
+                                "type = 'source')`."))
+  }
+  assign("has_data", has_data_package, envir = .pkgglobalenv)
 }
