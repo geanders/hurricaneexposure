e9e1d37007a6afc8b6ec04e454e4c1f326f64d09
Brooke Anderson <brooke.anderson@colostate.edu>
2017-05-01 10:36:39 -0600

    Change zzz.R back
    If we use `stop` in `hasData`, we get an error for examples if the package is checked in an R workspace that lacks the package you have in a drat repository. This means it would fail on CRAN checks.

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
     has_data <- requireNamespace("hurricaneexposuredata", quietly = TRUE)
     .pkgenv[["has_data"]] <- has_data
 }
-    
+
 .onAttach <- function(libname, pkgname) {
     if (!.pkgenv$has_data) {
         msg <- paste("To use this package, you must install the",
@@ -23,6 +23,7 @@ hasData <- function(has_data = .pkgenv$has_data) {
                      "`hurricaneexposuredata` package installed. See the",
                      "`hurricaneexposure` package vignette for more details.")
         msg <- paste(strwrap(msg), collapse="\n")
-        stop(msg)
+        message(msg)
+        return(invisible(NULL))
     }
 }
